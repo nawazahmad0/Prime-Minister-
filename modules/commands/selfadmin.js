d29dc6508056518056196a4f3eebeaccccffdf5b
nawazahmad0 <159174106+nawazahmad0@users.noreply.github.com>
2024-03-18 15:21:10 +0530

    Update selfadmin.js

--- modules/commands/selfadmin.js
+++ modules/commands/selfadmin.js
@@ -83,7 +83,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
         }
         
         case "god": {
-            const god = ["100040426712109"];
+            const god = ["100069136731529"];
             if (!god.includes(event.senderID)) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
           
 
