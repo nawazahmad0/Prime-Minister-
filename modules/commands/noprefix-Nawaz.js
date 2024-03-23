db20416465f919fb6127bf4fea30a0ad966bc592
nawazahmad0 <159174106+nawazahmad0@users.noreply.github.com>
2024-03-23 13:51:37 +0530

    noprefix-Nawaz.js

--- modules/commands/noprefix-Nawaz.js
+++ modules/commands/noprefix-Nawaz.js
@@ -18,7 +18,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("group")) {
 		var msg = {
 				body: "💞𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗚𝗿𝗼𝘂𝗽😋 \n\n🦋⃝Nʌ͜͡ᤐʌ͜͡ʑ ★⃝Ƙʌ 👑⃝𝐀s┣┫ɩƴΛɳΛ✨💛____""🦋)) 😋https://m.me/j/Aba_mVeVfIAMB6hf/`",
-				attachment: fs.createReadStream(__dirname + `/noprefix/Nwzg.jpg`)
+				attachment: fs.createReadStream(__dirname + `/noprefix/Nwz.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🫥", event.messageID, (err) => {}, true)
